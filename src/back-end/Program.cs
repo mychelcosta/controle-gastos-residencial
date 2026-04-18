121899b980cb4aca4b7144040b8b4220ea324ee9
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 20:52:13 -0300

    Adiciona Scalar e configura a documentação OpenAPI

--- src/back-end/Program.cs
+++ src/back-end/Program.cs
@@ -1,9 +1,36 @@
 using ApiFinanceira;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddOpenApi("v1", o =>
+{
+    o.AddDocumentTransformer((document, context, CancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "ApiFinanceira",
+            Description = "ApiFinanceira v1",
+            Version = "v1",
+        };
+        
+        document.Servers = 
+        [   
+            new() { Url = "http://localhost:5139" , Description = "Servidor Local" }
+        ];
+        
+        document.ExternalDocs = new()
+        {
+            Description = "Documentação externa",
+            Url = new Uri("https://github.com/mychelcosta/controle-gastos-residencial")
+        };
+        
+        return Task.CompletedTask;
+    });
+});
 
 builder.Services.AddControllers();
 
@@ -15,8 +42,14 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi("/docs/{documentName}.json");
 }
+
+app.MapScalarApiReference("/scalar", options =>
+{
+    options.WithOpenApiRoutePattern("/docs/{documentName}.json");
+    options.AddDocument("v1", "ApiFinanceira - v1");
+});
 
 app.UseHttpsRedirection();
 
