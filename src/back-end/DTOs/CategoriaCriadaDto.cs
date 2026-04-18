5b4208686bd43d031c58c61393488abae6ce5e32
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 19:22:24 -0300

    Cria o DTO para retornar resposta ao controlador Categoria

---
+++ src/back-end/DTOs/CategoriaCriadaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiFinanceira.DTOs;
+
+public class CategoriaCriadaDto
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public string Finalidade { get; set; } = string.Empty;
+}
