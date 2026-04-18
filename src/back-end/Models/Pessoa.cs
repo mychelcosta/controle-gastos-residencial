231dfdb7449532a0cc3bb5e1cc91b011129eb7cb
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 02:31:20 -0300

    Cria o modelo de dados de Pessoa

---
+++ src/back-end/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanceira.Models;
+
+public class Pessoa
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "É obrigatório informar o nome."),
+    MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
+    public required string Nome { get; set; }
+    [Required(ErrorMessage = "É obrigatório informar a idade.")]
+    public required int Idade { get; set; }
+}
