afd10088ba941f24e8e42b3580f25e668a3854c8
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 03:06:41 -0300

    Cria o modelo de dados de Transacao

--- src/back-end/Models/Pessoa.cs
+++ src/back-end/Models/Pessoa.cs
@@ -6,9 +6,11 @@ public class Pessoa
 {
     [Key]
     public int Id { get; set; }
+
     [Required(ErrorMessage = "É obrigatório informar o nome."),
-    MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
+        MaxLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres.")]
     public required string Nome { get; set; }
+    
     [Required(ErrorMessage = "É obrigatório informar a idade.")]
     public required int Idade { get; set; }
 }
---
+++ src/back-end/Models/Transacao.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanceira.Models;
+
+public class Transacao
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar a descrição."),
+        MaxLength(400, ErrorMessage = "A descrição deve ter no máximo 400 caracteres.")]
+    public required string Descricao { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar o valor.")]
+    public required decimal Valor { get; set; }
+
+    public Tipo Tipo { get; set; }
+}
+
+public enum Tipo
+{
+    Receita = 1,
+    Despesa = 2
+}
