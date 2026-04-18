10ac79f893fb4bafc4f8204668beaa2b8459b56c
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 02:43:07 -0300

    Cria o modelo de dados de Categoria

---
+++ src/back-end/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanceira.Models;
+
+public class Categoria
+{
+    [Key]
+     public int Id { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar a descrição."),
+        MaxLength(400, ErrorMessage = "A descrição deve ter no máximo 400 caracteres.")]
+     public required string Descricao { get; set; }
+
+    [Required(ErrorMessage = "É obrigatório informar a finalidade."),
+        EnumDataType(typeof(Finalidade), ErrorMessage = "Finalidade inválida.")]
+     public Finalidade Finalidade { get; set; }
+}
+
+public enum Finalidade
+{
+    Receita = 1,
+    Despesa = 2,
+    Ambas = 3
+}
