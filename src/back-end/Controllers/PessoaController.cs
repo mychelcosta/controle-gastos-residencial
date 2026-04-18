d5e5ba68ae816fc5b81796ed332357995b9b4211
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 12:01:48 -0300

    Cria o controlador Pessoa

---
+++ src/back-end/Controllers/PessoaController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiFinanceira;
+using ApiFinanceira.Models;
+
+namespace ApiFinanceira.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PessoaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pessoa
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoas()
+        {
+            return await _context.Pessoas.ToListAsync();
+        }
+
+        // GET: api/Pessoa/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pessoa>> GetPessoa(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return pessoa;
+        }
+
+        // PUT: api/Pessoa/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPessoa(int id, Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pessoa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PessoaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pessoa
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            _context.Pessoas.Add(pessoa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPessoa", new { id = pessoa.Id }, pessoa);
+        }
+
+        // DELETE: api/Pessoa/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePessoa(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
+        }
+    }
+}
