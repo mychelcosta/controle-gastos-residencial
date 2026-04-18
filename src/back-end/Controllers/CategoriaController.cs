644b043a6a701f5040241a978ca38e19fd64d1ff
Mychel Costa <91169665+mychelcosta@users.noreply.github.com>
2026-04-18 18:15:22 -0300

    Cria o controlador Categoria

---
+++ src/back-end/Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiFinanceira.Models;
+
+namespace ApiFinanceira.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: v1/Categoria
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        // POST: v1/Categoria
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoria", new { id = categoria.Id }, categoria);
+        }
+    }
+}
